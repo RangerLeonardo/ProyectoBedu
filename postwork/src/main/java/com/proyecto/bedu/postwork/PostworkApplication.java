86e798c445f5623f9d0a666f47ec4ba5bc73ea06
RangerLeonardo <brandonleonardobarrera@gmail.com>
2024-09-01 23:20:35 -0600

    Primer subida proyecto

---
+++ postwork/src/main/java/com/proyecto/bedu/postwork/PostworkApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.bedu.postwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostworkApplication.class, args);
+	}
+
+}
